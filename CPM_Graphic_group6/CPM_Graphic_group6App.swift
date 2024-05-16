3821d5a7ee5089312a0aabee41f26abccfc0ee9e
Yuntaek-Lee <ytyt0207@naver.com>
2024-05-16 11:11:00 +0900

    Initial Commit

---
+++ CPM_Graphic_group6/CPM_Graphic_group6App.swift
@@ -0,0 +1,17 @@
+//
+//  CPM_Graphic_group6App.swift
+//  CPM_Graphic_group6
+//
+//  Created by snlcom on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CPM_Graphic_group6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
